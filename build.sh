27a11882e29a55e5b91fd5d2fb35212a15d727b5
ercanemirleroglu <39986316+ercanemirleroglu@users.noreply.github.com>
2023-06-04 17:35:31 +0300

    build file

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+export PATH="/opt/homebrew/bin:$PATH"
 mvn clean install
 JAR_FILE="booking-tech-app/target/shameless-booking-tech.jar"
-DESTINATION="target/app.jar"
+DESTINATION="target/."
+mkdir target
 cp $JAR_FILE $DESTINATION
