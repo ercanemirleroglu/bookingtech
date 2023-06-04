a62f4bb2a1caf00b2223d562292cc5f7daa520ff
ercanemirleroglu <39986316+ercanemirleroglu@users.noreply.github.com>
2023-06-04 23:09:10 +0300

    dockerfile

--- booking-tech-integration/src/main/java/com/shamless/bookingtech/integration/automation/AutomationDriver.java
+++ booking-tech-integration/src/main/java/com/shamless/bookingtech/integration/automation/AutomationDriver.java
@@ -3,13 +3,21 @@ package com.shamless.bookingtech.integration.automation;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class AutomationDriver {
     protected static WebDriver driver;
 
     protected void executeDriverByPath(String path) {
         WebDriverManager.chromedriver().driverVersion("113.0.5672.63").setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        driver = new ChromeDriver(options);
         driver.get(path);
     }
 
