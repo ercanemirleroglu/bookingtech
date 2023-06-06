41d8dbc30d911885669139f01d1695baea294c89
ercanemirleroglu <39986316+ercanemirleroglu@users.noreply.github.com>
2023-06-06 19:43:22 +0300

    deneme

--- booking-tech-integration/src/main/java/com/shamless/bookingtech/integration/automation/service/BookingService.java
+++ booking-tech-integration/src/main/java/com/shamless/bookingtech/integration/automation/service/BookingService.java
@@ -243,7 +243,7 @@ public class BookingService {
     }
 
     private void closeRegisterModal() throws InterruptedException {
-        for (int i = 1000; i > 0; i--) {
+        for (int i = 10; i > 0; i--) {
             log.info("Clicking close register modal x button...");
             List<WebElement> xButtonInRegisterModal = operation.findElementsByCssSelector("button.fc63351294.a822bdf511.e3c025e003.fa565176a8.f7db01295e.c334e6f658.ae1678b153");
             if (xButtonInRegisterModal.size() == 1) {
