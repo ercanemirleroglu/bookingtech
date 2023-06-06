6626764f6e48f65b9c9ba528c6b21e8d60e91e85
ercanemirleroglu <39986316+ercanemirleroglu@users.noreply.github.com>
2023-06-06 17:36:28 +0300

    close register timeout

--- booking-tech-integration/src/main/java/com/shamless/bookingtech/integration/automation/AutomationDriver.java
+++ booking-tech-integration/src/main/java/com/shamless/bookingtech/integration/automation/AutomationDriver.java
@@ -20,7 +20,7 @@ public class AutomationDriver {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-debugging-address=0.0.0.0");
         options.addArguments("--remote-debugging-port=0");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         driver = new ChromeDriver(options);
         //driver.manage().window().maximize();
